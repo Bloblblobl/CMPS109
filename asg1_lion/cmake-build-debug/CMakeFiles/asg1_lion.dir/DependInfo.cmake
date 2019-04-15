9d751d40296872678ea5b2c6e56154dbc62231f3
Saar Sayfan <saarsayfan@gmail.com>
2019-04-14 18:43:46 -0700

    started asg1

---
+++ asg1_lion/cmake-build-debug/CMakeFiles/asg1_lion.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Blobl/Documents/Git/school/CMPS109/asg1_lion/main.cpp" "C:/Users/Blobl/Documents/Git/school/CMPS109/asg1_lion/cmake-build-debug/CMakeFiles/asg1_lion.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ asg1_lion/cmake-build-debug/CMakeFiles/asg1_lion.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/asg1_lion.dir/main.cpp.obj"
+  "asg1_lion.pdb"
+  "asg1_lion.exe"
+  "asg1_lion.exe.manifest"
+  "libasg1_lion.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/asg1_lion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
